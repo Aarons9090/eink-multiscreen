c2b4eccff019f67cbb1070d6d08ea8b2d8d13ad8
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-12-19 19:45:43 +0200

    add weather api

---
+++ api/weatherdata.py
@@ -0,0 +1,29 @@
+import requests
+
+
+def get_weatherdata():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=62.24&longitude=25.72&hourly=temperature_2m,apparent_temperature,precipitation,cloudcover,windspeed_10m&current_weather=true"
+
+    resp = requests.get(url)
+
+    data = resp.json()
+    hourly_data = data["hourly"]
+    current_time = data["current_weather"]["time"]
+    index = hourly_data["time"].index(current_time)
+
+    temp = hourly_data["temperature_2m"][index]
+    apparent_temperature = hourly_data["apparent_temperature"][index]
+    precipitation = hourly_data["precipitation"][index]
+    cloudcover = hourly_data["cloudcover"][index]
+    windspeed = hourly_data["windspeed_10m"][index]
+
+    return {
+        "temp": temp,
+        "apparent_temp": apparent_temperature,
+        "precipitation": precipitation,
+        "cloudcover": cloudcover,
+        "windspeed": windspeed
+    }
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import api.weatherdata as weather_api
+
+
+def main():
+    weather_api.get_weatherdata()
+
+
+main()

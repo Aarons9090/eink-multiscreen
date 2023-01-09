4ca12859e67af25294e8643986c4b22f0b8dbc22
Aaron <aaronhir00@gmail.com>
2023-01-09 20:44:41 +0200

    add ui elements for precipitation and wind speed + event loop

--- main.py
+++ main.py
@@ -18,30 +18,42 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
 
     try:
-        date = datetime.datetime.now()
-        datestr = date.strftime("%H:%M %A %d.%m.%Y")
-        weatherdata = weather_api.get_weatherdata()
-        temp = str(weatherdata["temp"])
        	epd = epd4in2.EPD()
         logging.info("init and Clear")
         epd.init()
         epd.Clear()
         print(os.path.join(miscdir, 'Font.ttc'))
 
-        font57 = ImageFont.truetype(os.path.join(miscdir, 'Font.ttc'), 57)
+        font53 = ImageFont.truetype(os.path.join(miscdir, 'Font.ttc'), 53)
         font24 = ImageFont.truetype(os.path.join(miscdir, 'Font.ttc'), 24)
         font18 = ImageFont.truetype(os.path.join(miscdir, 'Font.ttc'), 18)
         font35 = ImageFont.truetype(os.path.join(miscdir, 'Font.ttc'), 35)
 
-        Limage = Image.new('L', (epd.width, epd.height), "white")  # 255: clear the frame
-        draw = ImageDraw.Draw(Limage)
+        weather_test_icon = Image.open(os.path.join(miscdir, "weathertestbpm.bmp"))
+        wind_icon = Image.open(os.path.join(miscdir, "windbmp.bmp"))
+        drop_icon = Image.open(os.path.join(miscdir, "dropbmp.bmp"))
 
-        draw.rectangle((0,30,400,0), fill="black") 
-        draw.text((10, 0), datestr, font=font24, fill="white")
-        draw.text((121,39), f"{temp}°C", font=font57, fill="black")
+        while True:
+            date = datetime.datetime.now()
+            datestr = date.strftime("%H:%M %A %d.%m.%Y")
+            weatherdata = weather_api.get_weatherdata()
+            temp = str(weatherdata["temp"])
+            precipitation = str(weatherdata["precipitation"])
+            windspeed = str(weatherdata["windspeed"])
 
-        epd.display_4Gray(epd.getbuffer_4Gray(Limage))
-        time.sleep(5)
+            Limage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+            draw = ImageDraw.Draw(Limage)
+
+            draw.rectangle((0,30,400,0), fill="black") 
+            draw.text((10, 0), datestr, font=font24, fill="white")
+            Limage.paste(weather_test_icon, (7,41))
+            draw.text((115,45), f"{temp}°C", font=font53, fill="black")
+            Limage.paste(drop_icon, (282, 48))
+            draw.text((312,47), f"{precipitation}%", font=font24, fill="black") 
+            Limage.paste(wind_icon, (270, 82))
+            draw.text((312,77), f"{windspeed}m/s", font=font24, fill="black")
+            epd.display(epd.getbuffer(Limage))
+            time.sleep(60)
 
         epd.Clear()
         logging.info("Goto Sleep...")
@@ -52,6 +64,7 @@ def main():
 
     except KeyboardInterrupt:
         logging.info("ctrl + c:")
+        epd.Clear()
         epd4in2.epdconfig.module_exit()
         exit()
 

c77b312a25f91497b8be75a28721739279e43899
Aaron Hirvi <aaron.hirvi@tuni.fi>
2023-01-09 13:17:56 +0200

    added e-paper test functions

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
 import api.weatherdata as weather_api
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
 
+import logging
+from waveshare_epd import epd4in2
+import time
+from PIL import Image, ImageDraw, ImageFont
+import traceback
 
 def main():
-    weather_api.get_weatherdata()
+
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    try:
+        logging.info("epd4in2 Demo")
+
+        epd = epd4in2.EPD()
+        logging.info("init and Clear")
+        epd.init()
+        epd.Clear()
+
+        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+        font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+        font35 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 35)
+
+        Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(Himage)
+        draw.text((10, 0), 'hello world', font=font24, fill=0)
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(2)
+
+        epd.Clear()
+        logging.info("Goto Sleep...")
+        epd.sleep()
+
+    except IOError as e:
+        logging.info(e)
+
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd4in2.epdconfig.module_exit()
+        exit()
 
 
 main()
